86835e3a835ccc7b5b876d72e82f3a55cc1e88b2
Andrey Merkulov <merkulov.andrej@gmail.com>
2025-11-17 21:42:28 +0100

    Minor code cleanup

--- core/test-utils/src/main/java/com/iiddd/weather/core/testutils/UnitTestDispatcherProvider.kt
+++ core/test-utils/src/main/java/com/iiddd/weather/core/testutils/UnitTestDispatcherProvider.kt
@@ -1,10 +1,11 @@
 package com.iiddd.weather.core.testutils
 
 import com.iiddd.weather.core.utils.coroutines.DispatcherProvider
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 
-class UnitTestDispatcherProvider(
+class UnitTestDispatcherProvider @OptIn(ExperimentalCoroutinesApi::class) constructor(
     val dispatcher: TestDispatcher = UnconfinedTestDispatcher()
 ) : DispatcherProvider {
     override val main = dispatcher
