ea24e21934b3490d15bbb348a896b3b286538864
Andrey Merkulov <merkulov.andrej@gmail.com>
2026-01-26 17:57:57 +0100

    Minor cleanup

--- feature/settings/src/main/java/com/iiddd/weather/settings/presentation/view/component/ThemeCard.kt
+++ feature/settings/src/main/java/com/iiddd/weather/settings/presentation/view/component/ThemeCard.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
